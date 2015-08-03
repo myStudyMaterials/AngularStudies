7b8c58150af0e56f048ea621f66fec0be21ea8d8
alenjoy <alen.joy@inapp.com>
2015-08-03 15:30:08 +0530

    initial commit

---
+++ WorkingWithAngular_ng-include/defaultCtrl.js
@@ -0,0 +1,11 @@
+﻿app.controller("defaultCtrl", function ($scope) {
+
+    $scope.todos = [
+        { action: "Task One", complete: true },
+        { action: "Task Two", complete: false },
+        { action: "Task Three", complete: true },
+        { action: "Task Four", complete: false },
+        { action: "Task Five", complete: true }
+    ];
+
+});
